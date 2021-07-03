85c9b99c0761348ae658137655356989e37c9e32
KayJuno <45003518+dangdan2807@users.noreply.github.com>
2021-07-04 01:00:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,80 @@
-import requests
+import urllib.request
 import json
 import datetime
 import random
 import string
 import time
-
-referrer = input("id account cua bạn: ")
-#referrer = "87222f08-869c-4277-8831-cdee903acfce"
-timesToLoop = 99999999
-retryTimes = 5
-sleeptime = 20
-
+import os
+import sys
+os.system('cls' if os.name == 'nt' else 'clear')
+print ("[+] ABOUT SCRIPT:")
+print ("[-] With this script, you can getting unlimited GB on Warp+.")
+print ("[-] Version: 4.0.0")
+print ("--------")
+referrer = input("[#] Enter the WARP+ ID:")
 def genString(stringLength):
-    letters = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
-
-url = 'https://api.cloudflareclient.com/v0a848/reg'
-
-
+	try:
+		letters = string.ascii_letters + string.digits
+		return ''.join(random.choice(letters) for i in range(stringLength))
+	except Exception as error:
+		print(error)		    
+def digitString(stringLength):
+	try:
+		digit = string.digits
+		return ''.join((random.choice(digit) for i in range(stringLength)))    
+	except Exception as error:
+		print(error)	
+url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
 def run():
-    install_id = genString(11)
-    body = {"key": "{}=".format(genString(42)),
-            "install_id": install_id,
-            "fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
-            "referrer": referrer,
-            "warp_enabled": False,
-            "tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
-            "type": "Android",
-            "locale": "zh-CN"}
+	try:
+		install_id = genString(22)
+		body = {"key": "{}=".format(genString(43)),
+				"install_id": install_id,
+				"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
+				"referrer": referrer,
+				"warp_enabled": False,
+				"tos": datetime.datetime.now().isoformat()[:-3] + "+02:00",
+				"type": "Android",
+				"locale": "es_ES"}
+		data = json.dumps(body).encode('utf8')
+		headers = {'Content-Type': 'application/json; charset=UTF-8',
+					'Host': 'api.cloudflareclient.com',
+					'Connection': 'Keep-Alive',
+					'Accept-Encoding': 'gzip',
+					'User-Agent': 'okhttp/3.12.1'
+					}
+		req         = urllib.request.Request(url, data, headers)
+		response    = urllib.request.urlopen(req)
+		status_code = response.getcode()	
+		return status_code
+	except Exception as error:
+		print(error)	
 
-    bodyString = json.dumps(body)
-
-    headers = {'Content-Type': 'application/json; charset=UTF-8',
-               'Host': 'api.cloudflareclient.com',
-               'Connection': 'Keep-Alive',
-               'Accept-Encoding': 'gzip',
-               'User-Agent': 'okhttp/3.12.1'
-               }
-
-    r = requests.post(url, data=bodyString, headers=headers)
-    return r
-
-
-for i in range(timesToLoop):
-    result = run()
-    if result.status_code == 200:
-        print(i + 1, "Done: + 1 GB,   wait for 20 seconds :)")
-        time.sleep(sleeptime)
-    else:
-        print(i + 1, "Error")
-        for r in range(retryTimes):
-            retry = run()
-            if retry.status_code == 200:
-                print(i + 1, "Retry #" + str(r + 1), ", Done: + 1 GB,   wait for 20 seconds :)")
-                time.sleep(timesleep)
-                break
-            else:
-                print(i + 1, "Retry #" + str(r + 1), "Error")
-                if r == retryTimes - 1:
-                    exit()
+g = 0
+b = 0
+while True:
+	result = run()
+	if result == 200:
+		g = g + 1
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("")
+		print("                  WARP-PLUS-CLOUDFLARE (script)")
+		print("")
+		animation = ["[■□□□□□□□□□] 10%","[■■□□□□□□□□] 20%", "[■■■□□□□□□□] 30%", "[■■■■□□□□□□] 40%", "[■■■■■□□□□□] 50%", "[■■■■■■□□□□] 60%", "[■■■■■■■□□□] 70%", "[■■■■■■■■□□] 80%", "[■■■■■■■■■□] 90%", "[■■■■■■■■■■] 100%"] 
+		for i in range(len(animation)):
+			time.sleep(0.5)
+			sys.stdout.write("\r[+] Preparing... " + animation[i % len(animation)])
+			sys.stdout.flush()
+		print(f"\n[-] WORK ON ID: {referrer}")    
+		print(f"[:)] {g} GB has been successfully added to your account.")
+		print(f"[#] Total: {g} Good {b} Bad")
+		print("[*] After 18 seconds, a new request will be sent.")
+		time.sleep(18)
+	else:
+		b = b + 1
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("")
+		print("                  WARP-PLUS-CLOUDFLARE (script)")
+		print("")
+		print("[:(] Error when connecting to server.")
+		print(f"[#] Total: {g} Good {b} Bad")	
